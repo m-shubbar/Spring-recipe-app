a7f6f132029dfff0df2fe95659de30e6fd7c3d9a
Mustafa <codingbox@outlook.com>
2021-08-24 15:44:50 -0400

    Category entity & Difficulty enum added.

---
+++ src/main/java/ca/shubbar/recipeapp/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package ca.shubbar.recipeapp.domain;
+
+/**
+ * @author Mustafa <codingbox@outlook.com>
+ * Created at 2021-08-24
+ */
+public enum Difficulty {
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
